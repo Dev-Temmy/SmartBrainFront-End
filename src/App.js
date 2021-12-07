d7da59aebe221358d62241997c8e552242825e32
Dev-Temmy <callonyemmo@gmail.com>
2021-12-07 16:15:21 +1100

    adjusting changes in the development

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import Particles from 'react-particles-js';
 //import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
